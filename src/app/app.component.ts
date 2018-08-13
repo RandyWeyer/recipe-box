68ebcdfef24fdee188c8c705d39d7560515f7d7b
Randy Weyer <randy@>
2018-08-13 11:50:30 -0700

    Added more logic to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'recipe-box',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title: string = 'app works!';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  firstTask: Task = new Task("Finish weekend Angular homework for Epicodus course");
+}
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string) { }
 }
